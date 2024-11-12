739a901d42a1a6d7ee315b8f49525a97e45d6d8c
Marnix Oppeneer <marnix_oppeneer@hotmail.com>
2024-11-12 08:29:10 +0100

    t

--- flask_app.py
+++ flask_app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')  
 def hello_world():
-    return "<p>Hello world!</p>"  
+    return "<p>Les 11 huiswerk opdracht, kijken of meneer Declerck het goed vind :)</p>"  
